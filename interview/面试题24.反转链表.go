84b6eea4280c9e65fb6b21dc1a08c469f1544a45
cmatrixprobe <me@liushenghan.com>
2020-06-02 17:12:14 +0800

    循环反转链表

--- interview/面试题24.反转链表.go
+++ interview/面试题24.反转链表.go
@@ -10,6 +10,18 @@ func reverseList1(head *ListNode) *ListNode {
 	return res
 }
 
+func reverseList2(head *ListNode) *ListNode {
+	dummy := new(ListNode)
+	p := head
+	for p != nil {
+		q := p.Next
+		p.Next = dummy.Next
+		dummy.Next = p
+		p = q
+	}
+	return dummy.Next
+}
+
 func SolveOffer24() {
 	arr := []int{1, 2, 3, 4, 5}
 	head := CreateLinkedList(arr)
